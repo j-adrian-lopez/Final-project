23c50c52c244ce76e80c2c93c636f24abc161f43
j-adrian-lopez <adrj156@byui.edu>
2023-12-13 14:52:27 -0300

    Change localstorage

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ const lastScore = document.getElementById("last-score")
 
 if (!window.localStorage.getItem("username")) {
 	lastUser.textContent = "Waiting for the first Pokémon Master";
-    lastScore.textContent = 0; 
 } else {
     lastUser.textContent = window.localStorage.getItem("username");
-    lastScore.textContent = window.localStorage.getItem("score"); 
 }
+lastScore.textContent = window.localStorage.getItem("score"); 
 loadHeaderFooter();
